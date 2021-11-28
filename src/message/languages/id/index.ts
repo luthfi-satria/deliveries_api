b7a96fc195cfe78ad847db65e8abd159c841d159
saniakarinti <sani.paranta@akarinti.tech>
2021-11-28 08:58:47 +0700

    fix(deployment) fix sonarqube error

--- src/message/languages/id/index.ts
+++ src/message/languages/id/index.ts
@@ -3,7 +3,6 @@ import http from './http';
 import file from './file';
 import auth from './auth';
 import common from './common';
-import callback from './callback';
 
 export default {
   delivery,
@@ -11,5 +10,4 @@ export default {
   file,
   auth,
   common,
-  callback,
 };
