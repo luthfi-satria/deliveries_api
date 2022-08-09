dda43dbcbb8676aee47986bc70215e3e3640f91c
DESKTOP-3L8AFF1\Lenovo <saldhyyoga-akarinti>
2022-08-09 13:58:03 +0700

    feat/add body and method to save request,response,and callback from 3rd party

--- src/deliveries/deliveries.service.ts
+++ src/deliveries/deliveries.service.ts
@@ -184,7 +184,7 @@ export class DeliveriesService {
 
           this.saveNegativeResultOrder(deliveryData, err);
         });
-      const request = { header: headerRequest, url: urlDelivery };
+      const request = { header: headerRequest, url: urlDelivery, data: orderData, method: "POST" };
       this.thirdPartyRequestsRepository.save({
         request,
         response: orderDelivery,
@@ -316,7 +316,7 @@ export class DeliveriesService {
           ),
         );
       });
-    const request = { header: headerRequest, url: urlDelivery };
+    const request = { header: headerRequest, url: urlDelivery, data: data, method: "DELETE" };
     this.thirdPartyRequestsRepository.save({
       request,
       response: cancelOrderDelivery,
