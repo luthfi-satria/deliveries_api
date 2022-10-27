816d7060b164a0c0d71beb684d34d2a985b1d29f
Ardiansyahwelli <ardiansyahweli@Ardiansyahwelis-MacBook-Pro.local>
2022-10-27 10:29:21 +0700

    updated query param and condition search regionals elog

--- src/regionals/regionals.controller.ts
+++ src/regionals/regionals.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { RegionalsService } from './regionals.service';
 import { ResponseStatusCode } from 'src/response/response.decorator';
 import { RSuccessMessage } from 'src/response/response.interface';
@@ -6,6 +6,7 @@ import { MessageService } from 'src/message/message.service';
 import { ResponseService } from 'src/response/response.service';
 import { UserTypeAndLevel } from 'src/auth/guard/user-type-and-level.decorator';
 import { AuthJwtGuard } from 'src/auth/auth.decorators';
+import { RegionalsDTO } from './dto/regionals.dto';
 
 @Controller('api/v1/deliveries/elog')
 export class RegionalsController {
@@ -15,46 +16,13 @@ export class RegionalsController {
     private readonly regionalsService: RegionalsService,
   ) {}
 
-  @Get('/regionals/:name/:page/:limit')
+  @Get('/regionals')
   @UserTypeAndLevel('admin.*')
-  @AuthJwtGuard()
+  // @AuthJwtGuard()
   @ResponseStatusCode()
-  async regionalsElogByName(
-    @Param('name') name: string,
-    @Param('limit') limit: string,
-    @Param('page') page: string,
-  ): Promise<RSuccessMessage> {
+  async getAllRegionals(@Query() data: RegionalsDTO): Promise<RSuccessMessage> {
     try {
-      const elogRegionals = await this.regionalsService.listElogRegionals(
-        name,
-        limit,
-        page,
-      );
-      return this.responseService.success(
-        true,
-        this.messageService.get('delivery.general.success'),
-        elogRegionals,
-      );
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  }
-
-  @Get('/regionals/:page/:limit')
-  @UserTypeAndLevel('admin.*')
-  @AuthJwtGuard()
-  @ResponseStatusCode()
-  async getAllRegionals(
-    @Param('limit') limit: any,
-    @Param('page') page: any,
-  ): Promise<RSuccessMessage> {
-    try {
-      console.log('testting');
-      const elogRegionals = await this.regionalsService.getAllRegionals(
-        limit,
-        page,
-      );
+      const elogRegionals = await this.regionalsService.getAllRegionals(data);
       return this.responseService.success(
         true,
         this.messageService.get('delivery.general.success'),
