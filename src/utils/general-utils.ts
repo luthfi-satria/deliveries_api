a9ee89a86ec04655e185aa2cd0ba9974303d37d7
luthfisr <luthfi.ramadhan@edot.id>
2023-01-04 11:19:52 +0700

    remove unecessary return value

--- src/utils/general-utils.ts
+++ src/utils/general-utils.ts
@@ -238,5 +238,4 @@ export const diffInMinutes = function diffInMinutes(date1, date2) {
   const d1 = new Date(date1).getTime();
   const d2 = new Date(date2).getTime();
   return Math.abs(Math.round((((d2 - d1) % 86400000) % 3600000) / 60000));
-  return Math.abs(Math.round((d2 - d1) / 60000));
 };
